07b0b41d06ee804dc789ca08bdacfadc875629d2
Parv Gupta <149174587+Parv49@users.noreply.github.com>
2024-01-19 14:49:00 +0000

    Container Config Done

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,20 @@
 "use strict";
 // treat all JS code as newer vertion
 
+// Premitive Datatypes
+// String, number, null,boolen,null,undefined,symbol
+
+// Nonpremitive Datatype
+// objects
+
+
+let age=18;
+console.log(typeof"Parv")
+console.log(typeof age)
+// tell the type of datatype of the given argument
+
+console.log(typeof undefined)
+// iska datatype to undefine hi dikhata he
+
+console.log(typeof null)
+// iska dtatype object dikhata he esa kyo he ange dekhenge
