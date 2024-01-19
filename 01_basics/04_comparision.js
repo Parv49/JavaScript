af94b6f1dab37b56d862b6dcace475c84ef2f2da
Parv Gupta <149174587+Parv49@users.noreply.github.com>
2024-01-19 18:18:53 +0000

    Container Config Done

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -15,3 +15,14 @@ console.log(undefined<0)
 
 console.log("2" === 2)
 // ye strictly compare karta he jese ki agar sirf == lagao to vo to true dedega iss case me kiyoki vo khud convertion karlega aur 2=2 hoga to ture dedega lekin jo === he ye dekhega ki inke data type alag he to ye false dega
+
+
+// ********************************************MEMORY**********************************************
+
+// Memory r of 2 types: stack, Heap
+
+// stack: used in primitive data types
+// jab bhi stack memory use hoti he to jo bhi variable use kara he uski copy milty he , to agar baad me uss variable ki value change bhi kar rahe ho to vo uss copy me se change hoti he orignal same hi rheti he
+
+// Heap: used in Non-Primitive data types
+// jab bhi heap memory use hoti he to jo bhi variable use hota he uss ka orignal reference milta he to agar baad me usski value change bho kar rahe ho to uss ki orignal value bhi change ho jaegi
